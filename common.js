80549f5518a83702a774ce6e9b2bf1f640a4631e
kumar838 <31312812+kumar838@users.noreply.github.com>
2019-01-09 17:36:48 +0530

    Update common.js

--- common.js
+++ common.js
@@ -128,6 +128,40 @@ var sayHelloKelly = say.bind(person2);
 sayHelloJon(); // Hello Jon Kuperman
 sayHelloKelly(); // Hello Kelly King
 
+call vs apply:
+var sayHello = function(){
+    alert('Hello, ' + this.name);
+};
+
+var sayGoodbye = function(){
+    alert('Goodbye, ' + this.name);
+};
+
+var update = function(name, age, size){
+    this.name = name;
+    this.age = age;
+    this.size = size;
+};
+
+update.call(person1, 'Slarty', 200, '1xM');
+
+The limitations of call quickly become apparent when you want to write code that doesn't (or shouldn't)
+know the number of arguments that the functions need… like a dispatcher.
+
+So that's where apply comes in - the second argument needs to be an array, which is unpacked into arguments that are passed to the called function.
+
+So that's the difference between call and apply. Both can be called on functions, which they run in the context of the first argument. 
+In call the subsequent arguments are passed in to the function as they are, while apply expects the second argument to be an 
+array that it unpacks as arguments for the called function.
+
+var dispatch = function(person, method, args){
+    method.apply(person, args);
+};
+
+dispatch(person1, say, ['Hello']);
+dispatch(person2, update, ['Slarty', 200, '1xM']);
+
+---------------------**************-----------------------------------------------------------------------------
 `What is a closure?
 
 A closure is an inner function that has access to the outer (enclosing) function’s variables—scope chain. 
